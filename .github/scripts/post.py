b178773da241f5898588084c821d6b29fd1507a7
S I P U N <sipunkumar85@gmail.com>
2023-10-24 23:46:02 +0530

    script: Change UPI link
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- .github/scripts/post.py
+++ .github/scripts/post.py
@@ -168,7 +168,7 @@ def cook_content(information):
         "▫️ " + bold("Download: ", "<a href=\"https://www.pling.com/p/1908484\">Sourceforge</a>") + "\n" + \
         "▫️ " + bold("Changelog: ", "<a href=\"https://raw.githubusercontent.com/SuperiorOS-Devices/changelogs/thirteen/changelogs.txt\">Source</a>" + " | " + "<a href=\"https://raw.githubusercontent.com/SuperiorOS-Devices/changelogs/thirteen/thirteen_" + str(information['device']) + ".txt\"> Device</a>") + "\n" + \
         "▫️ " + bold("Flashing Method: ", "<a href=\"https://github.com/SuperiorOS-Devices/changelogs/blob/thirteen/flashing_method/" + str(information['device']) + ".md\">Here</a>") + "\n" + \
-        "▫️ " + bold("Donate: ", "<a href=\"https://www.paypal.me/Sipun\">Paypal</a>" + " | " + "<a href=\"https://telegra.ph/Donate-to-Darkstar085-08-31\"> UPI</a>") + "\n\n" + \
+        "▫️ " + bold("Donate: ", "<a href=\"https://www.paypal.me/Sipun\">Paypal</a>" + " | " + "<a href=\"https://drive.google.com/file/d/1JVR-pgbPzhFfJNRXojRzvfTO3Ouw2qvL\"> UPI</a>") + "\n\n" + \
         "#" + str(information['device']) + " | #besuperior | @superioros"
     return message
 
